9e318ec17ac01819680650b0c2d9912b1f5329b5
ZhangYiCheng <chengchengzhang123@qq.com>
2019-05-26 07:42:07 +0800

    更新

---
+++ project-cases/src/main/java/project/cases/CasesRunApplication.java
@@ -0,0 +1,12 @@
+package project.cases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasesRunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasesRunApplication.class, args);
+	}
+}
---
+++ project-cases/src/test/java/project/cases/CasesAllTest.java
@@ -0,0 +1,18 @@
+package project.cases;
+
+import org.junit.Test;
+
+public class CasesAllTest extends CasesApplicationTests {
+
+	/**
+	 *  测试"测试"的功能
+	 */
+	@Test
+	public void Test() {
+		System.out.println("第一个测试");
+	}
+
+
+
+
+}
